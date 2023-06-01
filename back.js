bd07672de506adf50a9a789cd3108bc4924a8940
minjun kim <9min3jun@naver.com>
2023-06-01 22:41:05 +0900

    finall really really
    ii sweaaaaaaar

--- back.js
+++ back.js
@@ -17,11 +17,13 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1', options
     .then(response => response.json())
     .then(data => {
         const movies = data.results;
-        // id 값으로 저장하기
+        // id 값으로 선택하기
         const container = document.getElementById('movieData'); // class 가 아니라 id 로 저장된다.
         // const container = document.getElementsByClassName('movieData'); // 이유는 모르겠는데 안된다... class로 했는데도 id값을 만들고 앉아있다.
-        
-        container.classList.add('movieData') // id랑 class를 동시에 가져도 문제 될 것이 없기때문에 그냥 class를 추가한다.
+
+        // id랑 class를 동시에 가져도 문제 될 것이 없기때문에 그냥 class를 추가한다.
+        // fetch 작업이 아니라 css 작업을 위해서 추가한 것이다.
+        container.classList.add('movieData')
 
         //  필수조건 forEach사용하기
         // 과제에서 필수조건으로 제시하지 않았더라도, 정보를 가공하는데는 반복 실행이 필요하다.
@@ -51,24 +53,37 @@ function handleSearch(event) {
     // 왜냐하면 같은 id값이 여러 군데에 할당되어 있다면 첫 번째 버튼의 값만 보기 때문이다.
     // 두 번째 버튼을 누르면 첫 번째 버튼의 id값을 없애도록 만들어 뒀기 때문에, 내가 어느 버튼을 눌렀는지 구분 할 수 있다.
     let typeBTN = document.getElementById("search_btn").textContent;
-    
+
     // console.log(typeBTN)
 
     if (typeBTN === '결과 내 검색') {
         // console.log('1111111111111111')
         if (searchTerm !== '') { // 검색어가 없는 경우에는 작동하지 않는다.
-            var container = document.getElementById('movieData');
-            var cards = container.getElementsByClassName('card');
-
+            let container = document.getElementById('movieData');
+            let cards = container.getElementsByClassName('card');
+            console.log(cards)
             Array.from(cards).forEach(function (card) {
-                var title = card.textContent.toLowerCase(); //영어 검색의 경우 대소문자 구분을 없애기 위해 전부 소문자 처리한다.
 
-                // 검색결과에 따라서 스타일을 추가한다.
-                if (title.includes(searchTerm)) {
-                    card.style.display = 'block'; //일치하는 카드를 보여주게 설정
-                } else {
-                    card.style.display = 'none'; //일치하지 않는 카드를 가리게 설정
+                let filteredCards = Array.from(cards).filter(function (filtering) { // filter 메서드에 보내는 콜백함수
+                    let filterdTitle = filtering.textContent.toLowerCase();
+                    return filterdTitle.includes(searchTerm);
+                });
+
+                if (filteredCards.includes(card) === false) {
+                    card.style.display = 'none';
                 }
+
+                
+                // includes를 사용하면 array를 새로 만들지 않고, filter, reduce, map, find를 사용하면 array를 만들기 때문에,
+                // includes가 더 경제적이라고 판단된다. 하지만 과제에서 요구한 조건에 includes가 없어서 filter로 구현했다. 
+               
+                // let title = card.textContent.toLowerCase(); //영어 검색의 경우 대소문자 구분을 없애기 위해 전부 소문자 처리한다.
+              
+                // 검색결과에 따라서 스타일을 추가한다.
+                // if (title.includes(searchTerm) === false) {
+                //     card.style.display = 'none';
+                //      //일치하지 않는 카드를 가리게 설정 // card.style.display = 'block'; //일치하는 카드를 보여주게 설정
+                // }
             });
         }
     } else {
@@ -113,18 +128,18 @@ function handleSearch(event) {
 function makeCards(movie) {
     const card = document.createElement('div'); // 카드들은 div 에 담는다.
     card.classList.add('card'); // class 명은 card 다
-  
+
     // 영화 제목은 h2 태그를 사용한다. h 태그는 단순히 크고 굵은 것이 아니라 정말로 제목이라는 의미이기 때문에 이것이 검색등에 중요한 역할을 한다.
-    const title = document.createElement('h2'); 
+    const title = document.createElement('h2');
     // 영화 제목과 평점이 같은 줄에 있어야 보기 편하기 때문에 평점도 같이 넣는다. 이때 제목과 평점의 구분을 편하게 하기 위해서 공백과 괄호를 넣는다.
     // 또한 api로 검색하면 소숫점 셋 째 자리까지 보여주기 때문에 .toFixed(1)로 소숫점 첫째자리와 가장 비슷한 숫자(str 타입)으로 변환한다.
-    title.textContent = movie.title + ' (' + movie.vote_average.toFixed(1) + ')'; 
-    title.classList.add('rate');
-  
+    title.textContent = movie.title + ' (' + movie.vote_average.toFixed(1) + ')';
+    // title.classList.add('rate');
+
     // 영화 개요는 단순히 텍스드들이 때문에 p 태그를 사용한다.
-    const overview = document.createElement('p'); 
+    const overview = document.createElement('p');
     overview.textContent = movie.overview;
-  
+
     //  필수조건 화살표 함수 사용하기
     // 포스터의 경우 크게 두가지 경우가 있다.
     // 1. 포스터 주소의 뒷부분(/고유값.jp)이 있는 경우
@@ -132,28 +147,28 @@ function makeCards(movie) {
     // null 값은 object 타입, 평범한 포스터들의 값은 string 타입이다.
     // string 타입이 아닌경우 위에서 선언해둔 TMDB의 로고 주소를 넣고,
     // 아닌 경우에는 w500 짜리 주소와 "조립" 한다.
-    const poster_front = movie.poster_path; 
+    const poster_front = movie.poster_path;
     const full = x => {
-      if (typeof x !== 'string') {
-        return poster_error;
-      } else {
-        return "https://image.tmdb.org/t/p/w500" + x;
-      }
+        if (typeof x !== 'string') {
+            return poster_error;
+        } else {
+            return "https://image.tmdb.org/t/p/w500" + x;
+        }
     }
-    
+
     //상기의 화살표 함수를 이용하여 포스터 주소를 불러온다.
     const poster = document.createElement('img');
     poster.src = full(poster_front);
     poster.classList.add('poster');
-  
+
     //위에서 만든 요소들을 카드에 추가한다.
     card.appendChild(title);
     card.appendChild(overview);
     card.appendChild(poster);
-    
+
     //만들어낸 카드를 내보낸다.
     return card;
-  }
+}
 
 
 
